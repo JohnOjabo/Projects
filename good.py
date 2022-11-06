8d1210779b9540f9308d5aadd48b3387703758f1
JohnOjabo <110981282+JohnOjabo@users.noreply.github.com>
2022-11-06 14:13:16 -0800

    Add files via upload

---
+++ good.py
@@ -0,0 +1,9 @@
+print("Hello there")
+name = input("What's your name ? ")
+age = input("How old are ? ")
+ages = int(age) * 365
+print("Hi " + name + " you have lived for " + str(ages) + " days.")
+sleep = 365
+truth = 90 * 365
+joyful_truth = truth - ages
+print(f"And you have ",joyful_truth," days to 90 years.")
