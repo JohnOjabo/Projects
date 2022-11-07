37cc55c3ae8d6c1d0d023dff04e22238f5a273ac
JohnOjabo <110981282+JohnOjabo@users.noreply.github.com>
2022-11-07 11:55:25 -0800

    Add files via upload

---
+++ conditionals.py
@@ -0,0 +1,20 @@
+print("Hey there, am a therapist Bot and am here to assist you get over psychological/mental problems ")
+print("Feel free to answer all the questions as you please.")
+name = input("What's your name? ")
+age = input("How old are you? ")
+mood = input("Do you often have mood swings? ")
+if mood == str():
+    print("It's okay to feel like that, as far as you are human ")
+else:
+    print("Don't worry, it will be fine ")
+ages = int(input("Am sorry, please how old are you again? "))
+if ages < 18:
+    print("All these might be due to puberty ")
+else:
+    print("You are already an adult, this should not be happening but, it's not that bad. ")
+summary = input("Please in a nutshell, describe the whole issue you are facing \n \n")
+if summary == str(""):
+    print("I understand all you just said, but dont worry, all will be well by God's Grace")
+else:
+    print("Am sorry i didn't get you ,,, please wait a moment while i check for higher matches, i will get back to you. ")
+print("Thank you for your time ")
